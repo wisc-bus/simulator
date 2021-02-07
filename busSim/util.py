8738f0e4a5d65bbdb997e0a993b77f845b752232
xcwisc <cxu249@wisc.edu>
2021-02-07 23:33:20 +0800

    add tool for parsing log file for performance

--- busSim/util.py
+++ busSim/util.py
@@ -1,6 +1,54 @@
+import pandas as pd
+from datetime import datetime
+
 debug = True
 
 
 def dprint(cmd):
     if debug:
         print(cmd)
+
+
+def parse_log_line(line):
+    line = line.strip()
+    tokens = line.split(" - ")
+    timestamp = datetime.strptime(tokens[0], "%Y-%m-%d %H:%M:%S,%f")
+    level = tokens[1]
+    msg = tokens[2]
+    return timestamp, level, msg
+
+
+def get_perf(log_path):
+
+    perf = pd.DataFrame()
+    graph_search = projections = uninioning = 0
+    graph_search_ts = projections_ts = uninioning_ts = None
+    i = 0
+
+    with open(log_path) as f:
+        for line in f:
+            timestamp, level, msg = parse_log_line(line)
+
+            if msg == "Start searching graph":
+                # print(1)
+                graph_search_ts = timestamp
+            elif msg == "start generating gdf":
+                # print(2)
+                projections_ts = timestamp
+                graph_search = projections_ts - graph_search_ts
+            elif msg == "start calculating union/difference":
+                # print(3)
+                uninioning_ts = timestamp
+                projections = uninioning_ts - projections_ts
+            elif msg == "finish calculating area":
+                # print(4)
+                uninioning = timestamp - uninioning_ts
+                # flush all previous results
+                perf.loc[i, "graph_search"] = graph_search
+                perf.loc[i, "projections"] = projections
+                perf.loc[i, "uninioning"] = uninioning
+                graph_search = projections = uninioning = 0
+                graph_search_ts = projections_ts = uninioning_ts = None
+                i += 1
+
+    return perf
